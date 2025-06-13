ae7f9cc6eda10b4673bf0aaf25c6755128eb8ea2
bhavya-bansal-29 <bhavyabansal1357@gmail.com>
2025-06-13 14:24:07 +0530

    change

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -8,7 +8,7 @@ export const BackgroundVideo = () => {
         playsInline
         className="w-full h-full object-cover brightness-[0.3]"
       >
-      <source src="/public/vd.mp4" type="video/mp4" />
+      <source src="/vd.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
